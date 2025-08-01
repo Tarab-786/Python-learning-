be68c9271058d9c222c2d49c5b1c273a145a6642
Tarab-786 <tarabtausi@gmail.com>
2025-08-01 19:33:04 +0530

    Update Variables.py

--- Basics/Variables.py
+++ Basics/Variables.py
@@ -9,4 +9,6 @@ print ("I'm 19 years old")
 #program 3: print message in same line 
 print ("I live in India.","I'm persuing B.Tech CSE")
 --------------------------------------------------------------------------------------------------------------------
-#program 4: 
+#program 4: printing of numbers 
+print (23) #you don't have to put those commas while printing number 
+print (23+48)
