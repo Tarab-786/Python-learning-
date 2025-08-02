58953e22d56e6825e1b3c05a831e76a2000a9465
Tarab-786 <tarabtausi@gmail.com>
2025-08-02 11:09:44 +0530

    Update Variables.py

--- Basics/Variables.py
+++ Basics/Variables.py
@@ -12,3 +12,21 @@ print ("I live in India.","I'm persuing B.Tech CSE")
 #program 4: printing of numbers 
 print (23) #you don't have to put those commas while printing number 
 print (23+48)
+--------------------------------------------------------------------------------------------------------------------
+#program 5: defining variables 
+name = "Tarab" #string
+age = 19 #integer(int)
+price = 60.57 #float
+
+print (name)
+print (age)
+print (price)
+--------------------------------------------------------------------------------------------------------------------
+#program 6: printing variables in sentence 
+name = "Tarab"
+age = 19
+price = 60.57
+
+print ("my name is:",name,".")
+print ("I'm",age,"years old.")
+print ("price of that book is:",price,"."
